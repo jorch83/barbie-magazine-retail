2105751421a07531dd1f4f98c9874820c1c2e434
bENon8318 <jorgetrujillo83@gmail.com>
2025-10-04 18:14:40 -0600

    update landings

---
+++ js/functions.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+  const logo = document.querySelector('.logo-todo-sobre-la-cerveza');
+  logo.classList.add('display-inline', 'animate__animated', 'animate__bounceInLeft');
+
+  const itemNav1 = document.querySelector('.item-nav-1');
+  itemNav1.classList.add('display-inline', 'animate__animated', 'animate__bounceInUp');
+  const itemNav2 = document.querySelector('.item-nav-2');
+  itemNav2.classList.add('display-inline', 'animate__animated', 'animate__bounceInDown');
+  const itemNav3 = document.querySelector('.item-nav-3');
+  itemNav3.classList.add('display-inline', 'animate__animated', 'animate__bounceInDown');
+
+};
+
+ 
